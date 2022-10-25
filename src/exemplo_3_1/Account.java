e11d706decf3533f0a0d7b05e600210636f75815
Marcel Colpani <marceluiz@gmail.com>
2022-10-25 19:34:36 -0300

    Exemplo 3.1

---
+++ src/exemplo_3_1/Account.java
@@ -0,0 +1,13 @@
+package exemplo_3_1;
+
+public class Account {
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
